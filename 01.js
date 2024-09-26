506f1d447ebeb3a97f9a3cf2116a6e8003484f8c
Jose Luis Criollo <joseluiscriollojlcr@gmail.com>
2024-09-26 07:47:22 -0500

    prueba

--- 01.js
+++ 01.js
@@ -1,8 +1,8 @@
-/* 
+/*
   Importante: 
   No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
-  código dentro de las funciones ya definidas. 
-  No comentar la funcion 
+  código dentro de las funciones ya definidas.
+  No comentar la funcion
 */
 
 function palabrasAFrase(array) {
